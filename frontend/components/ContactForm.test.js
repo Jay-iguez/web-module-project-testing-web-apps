0960c920c60f7ca68f64614289a9fccb36e119a0
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-08-26 21:02:38 -0700

    Create two test cases in ContactForm.test.js

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,11 +5,14 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-
+    render(<ContactForm />)
 });
 
 test('renders the contact form header', () => {
+    render(<ContactForm />)
+    const contactHeader = screen.getByText('Contact Form')
 
+    expect(contactHeader).toBeInTheDocument()
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
